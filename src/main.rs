212458f3a63e784d18d75efabf0ae6ff0f02910f
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2022-06-07 11:28:28 -0600

    Create pipe in a temporary file if path is unspecified

--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,66 @@ extern crate libc;
 use clap::{arg, command, Command};
 use nix::{sys::stat::Mode, unistd};
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+#[cfg(feature = "tempfile")]
+use tempfile::NamedTempFile;
 
 fn create_argparser() -> Command<'static> {
-    command!()
-        .arg(arg!([path] "The path to the named pipe that should be created").required(true))
-        .arg(
-            arg!(-t --threads "The number of worker threads to spawn")
-                .default_value("1")
-                .validator(|s| s.parse::<usize>())
-                .required(false),
-        )
+    let cmd = command!().arg(
+        arg!(-t --threads "The number of worker threads to spawn")
+            .default_value("1")
+            .validator(|s| s.parse::<usize>())
+            .required(false),
+    );
+
+    if cfg!(feature = "tempfile") {
+        let help = concat!(
+            "The path to the named pipe that should be created. ",
+            "If unset, a temporary file will be created for the pipe."
+        );
+
+        cmd.arg(arg!([path]).help(help).required(false))
+    } else {
+        cmd.arg(arg!([path] "The path to the named pipe that should be created.").required(true))
+    }
+}
+
+fn get_fifo_path(path: Option<&str>) -> PathBuf {
+    let path = if cfg!(feature = "tempfile") {
+        match path {
+            Some(path) => String::from(path),
+            None => {
+                let tf = NamedTempFile::new().unwrap();
+                let path = String::from(tf.path().to_str().unwrap());
+                println!("Creating FIFO at {}", path);
+                path
+            }
+        }
+    } else {
+        String::from(path.expect("required"))
+    };
+
+    Path::new(&path).to_owned()
+}
+
+fn create_fifo(path: &Path, mode: Option<Mode>) -> Result<()> {
+    let mode = mode.unwrap_or(Mode::all());
+    match unistd::mkfifo(path, mode) {
+        Err(e) => Err(ErrorKind::UnixError(e)),
+        _ => Ok(()),
+    }
 }
 
 fn main() -> Result<()> {
     let matches = create_argparser().get_matches();
-    let path = matches.value_of("path").expect("required");
-    let path = Path::new(&path);
+    let path = get_fifo_path(matches.value_of("path"));
+    let path = path.as_path();
     let display = path.display();
     let n_threads = matches.value_of_t("threads").expect("required");
 
     // Attempt to create the named pipe
-    let mode = Mode::all();
-    match unistd::mkfifo(path, mode) {
-        Err(e) => return Err(ErrorKind::UnixError(e)),
-        _ => {}
-    }
-
+    create_fifo(&path, None)?;
     let ret = stream::start_workers(&path, n_threads);
 
     println!("Closing stream...");
